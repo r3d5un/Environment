e304d1d51e371fc5476f901cfd2dfce05455dbe5
r3d5un <r3d5un@pm.me>
2024-12-08 00:14:19 +0100

    Create script for installing a Python development environment

---
+++ scripts/python.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$cwd/../utils/utils.sh"
+
+echo_banner "Python"
+
+log "INFO" "Installing Python versions 3.10 through 3.12"
+sudo dnf install -y \
+	python 3.10 \
+	python 3.11 \
+	python3.12
+
+log "INFO" "Installing Poetry package manager"
+curl -sSL https://install.python-poetry.org | python3 -
