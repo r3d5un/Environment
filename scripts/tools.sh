3d6e1872a7aa1e2859582095be5aeb5f319efcab
r3d5un <r3d5un@pm.me>
2024-12-08 02:08:35 +0100

    Create script for installing typical software for Linux

---
+++ scripts/tools.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$cwd/../utils/utils.sh"
+
+echo_banner "Tools"
+
+sudo dnf install -y \
+	alacritty \
+	btop \
+	chromium \
+	make \
+	git \
+	fd-find \
+	foot \
+	gcc \
+	gh \
+	htop \
+	kitty \
+	luarocks \
+	mangohud \
+	mpv \
+	neovim \
+	newsboat \
+	nodejs \
+	pass \
+	pavucontrol \
+	restic \
+	ripgrep \
+	snapd \
+	stow \
+	syncthing \
+	tokei \
+	wine \
+	winetricks \
+	zig
