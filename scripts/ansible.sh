217a3b264dea60dffaa664740576da52e0a49266
r3d5un <r3d5un@pm.me>
2024-12-10 16:00:07 +0100

    Create script for installing Ansible

---
+++ scripts/ansible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$cwd/../utils/utils.sh"
+
+echo_banner "ANSIBLE"
+
+require_root
+
+sudo dnf install -y ansible
