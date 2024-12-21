1f3295bb5d46df8a3be624dddfb1da461f75d67a
r3d5un <r3d5un@pm.me>
2024-12-22 00:10:42 +0100

    Set Neovim Obsidian note filenames configuraiton

--- dotfiles/nvim/lua/plugins/obsidian.lua
+++ dotfiles/nvim/lua/plugins/obsidian.lua
@@ -48,5 +48,16 @@ return {
 		attachments = {
 			img_folder = "imgs",
 		},
+		note_id_func = function(title)
+			local suffix = ""
+			if title ~= nil then
+				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+			else
+				for _ = 1, 4 do
+					suffix = suffix .. string.char(math.random(65, 90))
+				end
+			end
+			return tostring(os.time()) .. "-" .. suffix
+		end,
 	},
 }
