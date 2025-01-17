9836929fe01c6f7319960a04c3f0c1982db1fa27
r3d5un <r3d5un@pm.me>
2025-01-17 15:03:57 +0100

    Fix errors occurring when installing multiple Python versions

--- scripts/python.sh
+++ scripts/python.sh
@@ -7,8 +7,8 @@ echo_banner "PYTHON"
 
 log "INFO" "Installing Python versions 3.10 through 3.12"
 sudo dnf install -y \
-	python 3.10 \
-	python 3.11 \
+	python3.10 \
+	python3.11 \
 	python3.12
 
 log "INFO" "Installing Poetry package manager"
