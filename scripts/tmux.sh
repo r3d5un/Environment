0e03ccf187d6696c6c9552bf8811ad3f3396f68c
Øyvind Kristiansen <r3d5un@localhost.localdomain>
2025-03-11 18:43:19 +0100

    Update tmux script to be OpenSUSE compatible

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -7,7 +7,7 @@ echo_banner "TMUX"
 
 require_root
 
-sudo dnf install -y \
+sudo zypper --non-interactive install \
 	tmux \
 	git \
 	stow
@@ -19,12 +19,13 @@ if [ -e "$cwd" ]; then
 	log "INFO" "tpm already installed"
 else
 	log "INFO" "Installing TPM"
-	sudo -u r3d5un git clone https://github.com/tmux-plugins/tpm $tmux_dir/plugins/tpm
+	sudo -u r3d5un git clone https://github.com/tmux-plugins/tpm /home/r3d5un/.tmux/plugins/tpm
 fi
 
 log "INFO" "Sourcing configuration"
-sudo -u r3d5un tmux source $tmux_dir/plugins/tpm
+sudo -u r3d5un tmux source $tmux_dir/tmux.conf
 
 stow --verbose -d $cwd/../dotfiles -t /home/r3d5un/.config/tmux/ tmux
 
 echo -e "\n\nUse prefix + I to install plugins\n\n"
+
