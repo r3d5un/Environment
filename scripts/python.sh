0b614309541312ad7b70e58148ab46d2ff6d475a
Øyvind Kristiansen <r3d5un@localhost.localdomain>
2025-03-11 19:04:39 +0100

    Update python script to be OpenSUSE compatible

--- scripts/python.sh
+++ scripts/python.sh
@@ -5,11 +5,13 @@ source "$cwd/../utils/utils.sh"
 
 echo_banner "PYTHON"
 
-log "INFO" "Installing Python versions 3.10 through 3.12"
-sudo dnf install -y \
-	python3.10 \
-	python3.11 \
-	python3.12
+log "INFO" "Installing Python versions 3.10 through 3.13"
+sudo zypper --non-interactive install \
+	python310 \
+	python311 \
+	python312 \
+	python313
 
 log "INFO" "Installing Poetry package manager"
 sudo -u r3d5un curl -sSL https://install.python-poetry.org | python3 -
+
