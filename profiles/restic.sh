4fbc5a11ec6c468442dafd411dc3dd676ccab55e
Øyvind Kristiansen <r3d5un@localhost.localdomain>
2025-03-11 18:28:35 +0100

    Update restic script with new secret references

--- profiles/restic.sh
+++ profiles/restic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=$(pass digitalocean/restic/access_key_id | head -1)
-export AWS_SECRET_ACCESS_KEY=$(pass digitalocean/restic/access_key | head -1)
-export RESTIC_REPOSITORY=$(pass digitalocean/restic/repository | head -1)
-export RESTIC_PASSWORD=$(pass digitalocean/restic/password | head -1)
+export AWS_ACCESS_KEY_ID=$(pass hetzner/restic/access_key_id | head -1)
+export AWS_SECRET_ACCESS_KEY=$(pass hetzner/restic/access_key | head -1)
+export RESTIC_REPOSITORY=$(pass hetzner/restic/repository | head -1)
+export RESTIC_PASSWORD=$(pass hetzner/restic/password | head -1)
 export RESTIC_COMPRESSION="max"
 export RESTIC_PACK_SIZE="128"
