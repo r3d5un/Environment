c2c6d3645a048990ef899e2b8637b20b5e1beef8
Øyvind Kristiansen <r3d5un@localhost.localdomain>
2025-03-11 18:21:07 +0100

    WIP .NET SDK setup for OpenSUSE

--- scripts/dotnet.sh
+++ scripts/dotnet.sh
@@ -5,8 +5,24 @@ source "$cwd/../utils/utils.sh"
 
 echo_banner "#C/.NET"
 
-require_root
+log "INFO" ".NET install process not implemented"
 
-sudo dnf install -y \
-	dotnet-sdk-9.0 \
-	aspnetcore-runtime-9.0
+# TODO: The official .NET SDK requires libopenssl1_0_0, which is not provided in the OpenSUSE repositories.
+# An alternative route is to use distrobox.
+
+# require_root
+#
+# log "INFO" "Installing dependencies"
+# sudo zypper --non-interacive install libicu
+#
+# log "INFO" "Installing Microsoft repository"
+# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# wget https://packages.microsoft.com/config/opensuse/15/prod.repo
+# sudo mv prod.repo /etc/zypp/repos.d/microsoft-prod.repo
+# sudo chown root:root /etc/zypp/repos.d/microsoft-prod.repo
+#
+# log "INFO" "Installing .NET SDK and ASP.NET Core Runtime"
+# sudo zypper --non-interactive \
+# 	install dotnet-sdk-9.0 \
+# 	aspnetcore-runtime-9.0
+#
