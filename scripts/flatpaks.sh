cea5872d8e28b10f4403ff9e878e71db62d53e08
Øyvind Kristiansen <r3d5un@pm.me>
2025-03-11 18:37:09 +0100

    Update flatpak script to be OpenSUSE compatible

--- scripts/flatpaks.sh
+++ scripts/flatpaks.sh
@@ -7,33 +7,29 @@ echo_banner "FLATPAKS"
 
 require_root
 
-sudo dnf install flatpak
+sudo zypper --non-interactive install flatpak
 
-sudo -u r3d5un flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-sudo -u r3d5un flatpak install flathub -y \
+sudo flatpak install flathub -y \
 	com.bitwarden.desktop \
 	com.calibre_ebook.calibre \
 	com.discordapp.Discord \
 	com.github.johnfactotum.Foliate \
 	com.github.tchx84.Flatseal \
-	com.logseq.Logseq \
 	com.obsproject.Studio \
-	com.spotify.Client \
 	com.valvesoftware.Steam \
 	md.obsidian.Obsidian \
 	net.ankiweb.Anki \
 	net.lutris.Lutris \
 	org.filezillaproject.Filezilla \
-	org.gnome.Extensions \
-	org.gnome.Extensions \
 	org.mozilla.Thunderbird \
 	org.qbittorrent.qBittorrent \
 	org.remmina.Remmina \
 	org.signal.Signal \
 	org.torproject.torbrowser-launcher \
 	rest.insomnia.Insomnia \
-	sh.ppy.osu \
 	us.zoom.Zoom
 
-sudo -u r3d5un flatpak update -y
+sudo flatpak update -y
+
