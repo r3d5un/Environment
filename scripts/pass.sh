93e20dfd9b0e5fd7b55daa9cfb1b78744540df6e
Øyvind Kristiansen <r3d5un@pm.me>
2025-03-13 18:00:24 +0100

    Comment out pass script

--- scripts/pass.sh
+++ scripts/pass.sh
@@ -28,30 +28,30 @@ sudo zypper --non-interactive install \
 #
 # This script assumes that the keys are found in the secrets directory
 
-secrets_dir="$cwd/../secrets"
-log "INFO" "Checking for PGP keys in $secrets_dir"
-if [[ ! -e "$secrets_dir/public.pgp" || ! -e "$secrets_dir/private.pgp" ]]; then
-	log "ERROR" "private or public key not found in $secrets_dir"
-	exit 1
-else
-	log "INFO" "Importing private PGP key"
-	sudo -u r3d5un gpg --import $secrets_dir/private.pgp
-
-	log "INFO" "Importing public PGP key"
-	sudo -u r3d5un gpg --import $secrets_dir/public.pgp
-fi
-
-light_gray='\033[0m'
-no_color='\033[0m'
-
-echo -e "\n\nIn order for password store to work you need to trust the key\n"
-echo -e "Run: ${light_gray}gpg --edit-key email@address.io${no_color}\n"
-echo -e "Run the trust command, e.g.\n"
-echo -e "${light_gray}gpg> trust${no_color}\n"
-echo -e "Set trust level to the highest possible (5) when prompted:\n"
-echo -e "${light_gray}Your decision? 5${no_color}\n"
-echo -e "Quit and save:\n"
-echo -e "${light_gray}quit${no_color}\n\n"
-
-sudo -u r3d5un pass
-
+# secrets_dir="$cwd/../secrets"
+# log "INFO" "Checking for PGP keys in $secrets_dir"
+# if [[ ! -e "$secrets_dir/public.pgp" || ! -e "$secrets_dir/private.pgp" ]]; then
+# 	log "ERROR" "private or public key not found in $secrets_dir"
+# 	exit 1
+# else
+# 	log "INFO" "Importing private PGP key"
+# 	sudo -u r3d5un gpg --import $secrets_dir/private.pgp
+#
+# 	log "INFO" "Importing public PGP key"
+# 	sudo -u r3d5un gpg --import $secrets_dir/public.pgp
+# fi
+#
+# light_gray='\033[0m'
+# no_color='\033[0m'
+#
+# echo -e "\n\nIn order for password store to work you need to trust the key\n"
+# echo -e "Run: ${light_gray}gpg --edit-key email@address.io${no_color}\n"
+# echo -e "Run the trust command, e.g.\n"
+# echo -e "${light_gray}gpg> trust${no_color}\n"
+# echo -e "Set trust level to the highest possible (5) when prompted:\n"
+# echo -e "${light_gray}Your decision? 5${no_color}\n"
+# echo -e "Quit and save:\n"
+# echo -e "${light_gray}quit${no_color}\n\n"
+#
+# sudo -u r3d5un pass
+#
