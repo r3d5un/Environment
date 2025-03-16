c10ca760d218c1c47d7b217dc964cb5dbb96ef6f
r3d5un <r3d5un@pm.me>
2025-03-16 09:26:34 +0100

    Create script for installing Ghostty

---
+++ scripts/ghostty.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$cwd/../utils/utils.sh"
+
+echo_banner "GHOSTTY"
+
+font_path=/tmp/iosevka.zip
+if [ -e "$font_path" ]; then
+	log "INFO" "$font_path already exists"
+else
+	log "INFO" "Downloading fonts"
+	curl -L -o $font_path https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Iosevka.zip
+fi
+
+log "INFO" "Unpacking zipped directory"
+sudo unzip -o $font_path -d /usr/share/fonts/iosevka-nerd-font
+
+log "INFO" "Refreshing font cache"
+fc-cache -f
+
+log "INFO" "Installing kitty and dependencies"
+sudo zypper --non-interactive install \
+	ghostty \
+	stow
+
+log "INFO" "Making configuration directory"
+sudo -u r3d5un mkdir -p /home/r3d5un/.config/ghostty
+
+log "INFO" "Stowing configuration"
+stow --verbose -d $cwd/../dotfiles -t /home/r3d5un/.config/ghostty/ foot
