609357919377cf58677340e323c06d90b8ba9844
Øyvind Kristiansen <r3d5un@pm.me>
2025-03-16 09:59:13 +0100

    Fix errors in Zig install script

--- scripts/zig.sh
+++ scripts/zig.sh
@@ -2,6 +2,7 @@
 
 version="0.14.0"
 tarball=zig-linux-x86_64-$version.tar.xz
+unpacked_tarball=zig-linux-x86_64-$version
 tarball_path=/tmp/$tarball
 url=https://ziglang.org/download/$version/$tarball
 zig_dir=/usr/local/zig
@@ -16,8 +17,11 @@ require_root
 log "INFO" "Downloading Zig $version: $url"
 curl -L -o $tarball_path $url
 
-log "INFO" "Installing Zig"
-sudo mkdir -p $zig_dir
-sudo rm -rf $zig_dir && tar -C /usr/local -xzf $tarball_path
+log "INFO" "Removing existing Zig install"
+sudo rm -rf $zig_dir
 
-zig version
+log "INFO" "Unpacking tarball"
+sudo tar -C /tmp/ -xJf $tarball_path
+
+log "INFO" "Installing Zig"
+sudo mv /tmp/$unpacked_tarball $zig_dir
