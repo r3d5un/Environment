3c2bd970b18b6eb846fb1a0d97c109dcee9af62d
r3d5un <r3d5un@pm.me>
2025-04-01 22:19:50 +0200

    Fix broken install of node.js

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -6,4 +6,4 @@ source "$cwd/../utils/utils.sh"
 echo_banner "NODEJS"
 
 log "INFO" "Installing Node Version Manager"
-sudo -u r3d5un curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+sudo -u r3d5un curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | sudo -u r3d5un bash
