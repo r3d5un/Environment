0ff28878f6cdd5e8a5c46b44a56d82481e95b810
r3d5un <r3d5un@pm.me>
2025-04-05 20:21:48 +0200

    Add PostgreSQL Kubernetes manifests for a homelab setup

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -33,5 +33,11 @@ sudo usermod -G docker -a r3d5un
 sudo systemctl restart docker
 
 log "INFO" "Checking Minikube status"
-minikube start --driver=docker
+sudo -u r3d5un minikube start --driver=docker
+
+log "INFO" "Ensuring hostpath PV directories exists"
+sudo -u r3d5un mkdir -p /home/r3d5un/Development/Kubernetes/Data/pv-postgresql
+
+log "INFO" "Applying PostgreSQL manifest files"
+sudo -u r3d5un kubectl apply -f $cwd/../kube/postgresql/deployment.yaml
 
