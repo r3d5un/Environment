6b6e29c2f3d1e1f4c6a01c6c94bff3edd060a6cf
r3d5un <r3d5un@pm.me>
2025-05-30 12:34:04 +0200

    Update the .NET install script

--- scripts/dotnet.sh
+++ scripts/dotnet.sh
@@ -10,19 +10,15 @@ require_root
 # NOTE: The official .NET SDK requires libopenssl1_0_0, which is not provided in the OpenSUSE repositories. \
 # A Distrobox setup based on Fedora is used as an alternative.
 
-log "INFO" "Installing Distrobox dependency"
-sudo zypper install \
-	podman \
-	distrobox
+log "INFO" "Installing the Microsoft package repository"
+sudo zypper install libicu
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+wget https://packages.microsoft.com/config/opensuse/15/prod.repo
+sudo mv prod.repo /etc/zypp/repos.d/microsoft-prod.repo
+sudo chown root:root /etc/zypp/repos.d/microsoft-prod.repo
 
-log "INFO" "Creating dotnet distrobox container"
-sudo -u r3d5un distrobox create \
-	--name dotnet \
-	--image fedora:latest \
-	--additional-packages "java-latest-openjdk-devel.x86_64 dotnet-sdk-9.0 git"
+log "INFO" "Installing .NET SDK"
+sudo zypper install dotnet-sdk-9.0
 
-log "INFO" "Updating container packages"
-sudo -u r3d5un distrobox enter dotnet -- sudo dnf update -y
+dotnet --info
 
-log "INFO" "Running dotnet CLI to verify install"
-sudo -u r3d5un distrobox enter dotnet -- dotnet --info
