5db5ef2c799b4680dbf2ce06d0daed35af381cc8
r3d5un <r3d5un@pm.me>
2025-07-05 20:15:54 +0200

    Create script for installing a protobuf compiler

---
+++ scripts/protobuf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$cwd/../utils/utils.sh"
+
+echo_banner "PROTOBUF"
+
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+
+log "INFO" "Downloading protobufc"
+sudo -u r3d5un curl -L -o "/tmp/protoc.zip" "$PB_REL/download/v30.2/protoc-30.2-linux-x86_64.zip"
+
+log "INFO" "Unpacking zipped directory"
+sudo unzip "/tmp/protoc.zip" -d "/usr/local"
+
