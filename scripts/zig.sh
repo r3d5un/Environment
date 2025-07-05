9f2e03b636116fa46beb94e2eba347c5f9829cd6
r3d5un <r3d5un@pm.me>
2025-07-05 15:57:06 +0200

    Rewrite the Zig install script

--- scripts/zig.sh
+++ scripts/zig.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-version="0.14.0"
-tarball=zig-linux-x86_64-$version.tar.xz
-unpacked_tarball=zig-linux-x86_64-$version
-tarball_path=/tmp/$tarball
-url=https://ziglang.org/download/$version/$tarball
-zig_dir=/usr/local/zig
-
 cwd="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "$cwd/../utils/utils.sh"
 
@@ -14,14 +7,8 @@ echo_banner "ZIG"
 
 require_root
 
-log "INFO" "Downloading Zig $version: $url"
-curl -L -o $tarball_path $url
-
-log "INFO" "Removing existing Zig install"
-sudo rm -rf $zig_dir
-
-log "INFO" "Unpacking tarball"
-sudo tar -C /tmp/ -xJf $tarball_path
-
 log "INFO" "Installing Zig"
-sudo mv /tmp/$unpacked_tarball $zig_dir
+sudo zypper --non-interactive install zig
+
+zig zen
+
