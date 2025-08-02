0d381cd855441b546cd529745cd54f5e18e73834
r3d5un <r3d5un@pm.me>
2025-08-02 22:21:36 +0200

    Ensure LSP servers are installed

--- dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
@@ -108,15 +108,6 @@ return { -- LSP Configuration & Plugins
 			gopls = {},
 			pyright = {},
 			rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`tsserver`) will work just fine
-			-- tsserver = {},
-			--
-
 			lua_ls = {
 				-- cmd = {...},
 				-- filetypes { ...},
@@ -158,6 +149,27 @@ return { -- LSP Configuration & Plugins
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua", -- Used to format lua code
+			"ansible-language-server",
+			"bash-language-server",
+			"eslint-lsp",
+			"gopls",
+			"html-lsp",
+			"htmx-lsp",
+			"lua-language-server",
+			"marksman",
+			"protols",
+			"buf",
+			"pyright",
+			"ruff",
+			"rust-analyzer",
+			"typescript-language-server",
+			"vue-language-server",
+			"css-lsp",
+			"zls",
+			"dockerfile-language-server",
+			"json-lsp",
+			"jq-lsp",
+			"powershell-editor-services",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
