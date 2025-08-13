f9797da63458a64c98bb82b120c72fb5b824fec5
r3d5un <r3d5un@pm.me>
2025-08-13 14:42:46 +0200

    Add Vue treesitter

--- dotfiles/nvim/lua/plugins/nvim-treesitter.lua
+++ dotfiles/nvim/lua/plugins/nvim-treesitter.lua
@@ -33,6 +33,7 @@ return { -- Highlight, edit, and navigate code
 				"c_sharp",
 				"java",
 				"toml",
+				"vue",
 			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
