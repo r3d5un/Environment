687e1bdcb4240ed93958cd5be6cc260b92656b97
r3d5un <r3d5un@pm.me>
2025-10-19 11:22:47 +0200

    Install Docker on the work laptop

--- work.sh
+++ work.sh
@@ -8,7 +8,6 @@ sudo apt install -y \
 	btop \
 	make \
 	git \
-	fd \
 	gcc \
 	htop \
 	mpv \
@@ -26,13 +25,38 @@ sudo apt install -y \
 	eza \
 	bat \
 	entr \
-	lazygit \
-	bitwarden \
-	discord \
 	foliate \
 	lutris \
 	filezilla \
-	MozillaThunderbird \
 	kamoso \
-	remmina
+	remmina \
+	fd-find \
+	ca-certificates \
+	curl
 
+log "INFO" "Setting up Docker"
+sudo sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+sudo apt install -y \
+	docker-ce \
+	docker-ce-cli \
+	containerd.io \
+	docker-buildx-plugin \
+	docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo usermod -G docker -a r3d5un
+
+sudo systemctl restart docker
+
+docker run --rm hello-world
