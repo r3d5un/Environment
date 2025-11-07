a0f287cb927f77389893c48c5f1251f92c1d2da1
r3d5un <r3d5un@pm.me>
2025-11-07 20:33:25 +0100

    Install PostgreSQL on the work laptop

--- work.sh
+++ work.sh
@@ -196,3 +196,14 @@ sudo apt install -y steam-installer
 log "INFO" "Setting up Kubernetes"
 sudo snap install microk8s --classic
 
+log "INFO" "Installing PostgreSQL"
+sudo apt install curl ca-certificates
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+
+. /etc/os-release
+sudo sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+
+sudo apt update
+
+sudo apt install postgresql-18
