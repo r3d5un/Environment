8a842e7231471acc9fad21fc093f252cc73ef60d
r3d5un <r3d5un@pm.me>
2026-01-04 01:09:42 +0100

    Install the Mullvad VPN client

--- work.sh
+++ work.sh
@@ -235,3 +235,9 @@ sudo -u r3d5un curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/ins
 log "INFO" "Installing Vivaldi"
 sudo snap install vivaldi
 
+log "INFO" "Installing Mullvad VPN"
+sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable stable main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+sudo apt update
+sudo apt install mullvad-vpn
+
