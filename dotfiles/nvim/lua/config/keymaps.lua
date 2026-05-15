b712032f462d38ed13990ed8a76374e7b7dad71f
r3d5un <r3d5un@pm.me>
2026-05-15 19:12:57 +0200

    Add keymap for deleting all buffers

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -30,6 +30,7 @@ map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 -- by cycling to the previous buffer, then closing the buffer you were just on. This keeps the
 -- pane open, while closing the buffer.
 map("n", "<leader>bx", ":bp|bd #<cr>", { desc = "Delete Buffer" })
+map("n", "<leader>bX", ":%bd<cr>", { desc = "Delete all buffers" })
 map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
