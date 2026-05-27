4dcf4aee38ff48df4c5da1303695ca916f622052
r3d5un <r3d5un@pm.me>
2026-05-27 21:15:19 +0200

    Format themery.lua

--- dotfiles/nvim/lua/plugins/themery.lua
+++ dotfiles/nvim/lua/plugins/themery.lua
@@ -3,7 +3,13 @@ return {
 	lazy = false,
 	config = function()
 		require("themery").setup({
-			themes = { "tokyonight-night", "tokyonight-storm", "tokyonight-day", "tokyonight-moon", "default" },
+			themes = {
+				"tokyonight-night",
+				"tokyonight-storm",
+				"tokyonight-day",
+				"tokyonight-moon",
+				"default",
+			},
 			livePreview = true,
 		})
 	end,
